2abd5846ec1592e476045348df3e5514937db75e
Leonti <leonti.gold@gmail.com>
2023-06-13 08:46:06 +0300

    HEALTH-42 <Fix Output>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 
+    <h2 class="text-center">SPTV20 Healthy Lifestyle</h2>
     <ul class="nav justify-content-center">
         <li class="nav-item">
             <a class="nav-link active" href="/">Main</a>
